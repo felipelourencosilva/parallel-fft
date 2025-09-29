a2936afe59c1c359f2c96f0717af08a10b2c6236
Felipe Lourenco <lipelourencosilva@gmail.com>
2025-09-29 20:56:19 -0300

    Initial commit

---
+++ naive/naive.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e3;
+int result[MAXN];
+
+signed main() {
+    ios::sync_with_stdio(0);cin.tie(0);
+
+    memset(result, 0, sizeof(result));
+
+    for(int i = 0; i < MAXN; i++) {
+        cout << result[i] << " ";
+    }
+    
+    cout << "\n";
+    return 0;
+}
