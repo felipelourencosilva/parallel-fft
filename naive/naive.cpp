51327118c56033e24ab3e0b055df8855b826ec5d
Felipe Lourenco <lipelourencosilva@gmail.com>
2025-09-29 21:47:32 -0300

    Rand Coeff function

--- naive/naive.cpp
+++ naive/naive.cpp
@@ -3,25 +3,18 @@ using namespace std;
 
 signed main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    random_device rd;
-    mt19937 gen(rd());
 
     // n: degree of first polynomial
     // m: degree of second polynomial
-    // maxRand: max value for the coefficients
-    int n, m, maxRand; cin >> n >> m >> maxRand;
-    int minRand = 1;
-
-    uniform_int_distribution<int> distribution(minRand, maxRand);
-
+    int n, m, sz; cin >> n >> m >> sz;
     vector<int> a(n+1), b(m+1);
-    for(int &i : a) i = distribution(gen);
-    for(int &i : b) i = distribution(gen);
+    for (int &i : a) cin >> i;
+    for (int &i : b) cin >> i;
 
     cout << "a = "; for (int i : a) cout << i << " "; cout << endl;
     cout << "b = "; for (int i : b) cout << i << " "; cout << endl;
 
-    vector<int> c(n+m+1);
+    vector<int> c(sz);
     for (int i = 0; i < a.size(); i++)
         for (int j = 0; j < b.size(); j++)
             c[i+j] += a[i] * b[j];
---
+++ utils/randomCoeff.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    random_device rd;
+    mt19937 gen(rd());
+
+    // n: degree of first polynomial
+    // m: degree of second polynomial
+    // maxRand: max value for the coefficients
+    int n, m, maxRand; cin >> n >> m >> maxRand;
+    int minRand = 1;
+
+    uniform_int_distribution<int> distribution(minRand, maxRand);
+
+    vector<int> a(n+1), b(m+1);
+    for(int &i : a) i = distribution(gen);
+    for(int &i : b) i = distribution(gen);
+
+    cout << n << " " << m << " " << m+n+1 << endl;
+    for (int i : a) cout << i << " "; cout << endl;
+    for (int i : b) cout << i << " "; cout << endl;
+
+    return 0;
+}
