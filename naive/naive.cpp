c1e72e7ee6dbad2f4d6bddf3942933f1a64e07dd
Felipe Lourenco <lipelourencosilva@gmail.com>
2025-09-29 21:33:58 -0300

    Random coefficients generator

--- naive/naive.cpp
+++ naive/naive.cpp
@@ -2,23 +2,29 @@
 using namespace std;
 
 signed main() {
-    cin.tie(0)->sync_with_stdio(0);
+    ios::sync_with_stdio(0); cin.tie(0);
+    random_device rd;
+    mt19937 gen(rd());
 
     // n: degree of first polynomial
     // m: degree of second polynomial
-    int n, m; cin >> n >> m;
+    // maxRand: max value for the coefficients
+    int n, m, maxRand; cin >> n >> m >> maxRand;
+    int minRand = 1;
+
+    uniform_int_distribution<int> distribution(minRand, maxRand);
+
     vector<int> a(n+1), b(m+1);
-    for (int &i : a) cin >> i;
-    for (int &i : b) cin >> i;
+    for(int &i : a) i = distribution(gen);
+    for(int &i : b) i = distribution(gen);
 
     cout << "a = "; for (int i : a) cout << i << " "; cout << endl;
     cout << "b = "; for (int i : b) cout << i << " "; cout << endl;
 
-    vector<int> c(a.size()+b.size()-1);
+    vector<int> c(n+m+1);
     for (int i = 0; i < a.size(); i++)
         for (int j = 0; j < b.size(); j++)
             c[i+j] += a[i] * b[j];
-
 
     cout << "c = "; for (int i : c) cout << i << " "; cout << endl;
 
