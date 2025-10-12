e9f14699636b04ccc1fc3d4f35ac2e9e8f6cff83
Gabriel Guimarães <gabrielcastroguimaraes1@gmail.com>
2025-10-12 16:59:38 -0300

    feat: fixed parallel naive

--- naive/parallel_naive.cpp
+++ naive/parallel_naive.cpp
@@ -14,45 +14,51 @@ we will compute, for each position 'k', how many pairs (i, j) exist such that i+
 const int NUM_THREADS = 8;
 pthread_t threads[NUM_THREADS];
 vector<int> a, b, c;
+vector<pair<int, int>> work;
 
 // computes multiplication on range [l, r) of 'c'
 void* f(void* arg) {
     auto[l, r] = *static_cast<pair<int, int>*>(arg);
     for (int k = l; k < r; k++)
-        for (int i = 0; i <= k; i++)
+        for (int i = work[k].first; i <= work[k].second; i++)
             c[k] += a[i] * b[k-i];
     return nullptr;
 }
 
-vector<pair<int, int>> getPartitions() {
+int computeWork() {
     assert(a.size());
     int n = a.size();
     int m = b.size();
-    if (NUM_THREADS == 1) return {{0, n+m-1}};
-    vector<int> work(n+m-1);
+    work.assign(n+m-1, {0, -1});
 
     // work[k]: how many pairs (i,j) satisfy i+j=k
-    long long sum = 0;
+    int sum = 0;
     for (int k = 0; k < work.size(); k++) {
         // smallest 'i' such that there is a 'j' such that i+j==k
         int l = min(k, n-1);
         int r = min(k, m-1);
         int smallest = k-r;
         int largest = l;
-        if (smallest <= largest)
-            work[k] = largest - smallest + 1;
-        sum += work[k];
+        if (smallest <= largest) {
+            work[k] = {smallest, largest};
+            sum += largest - smallest + 1;
+        }
     }
 
-    // NUM_THREAD threads must execute 'sum' iterations
+    return sum;
+}
+
+vector<pair<int, int>> getPartitions(int sum) {
     long long target = (sum+NUM_THREADS-1) / NUM_THREADS;
     vector<pair<int, int>> ret(NUM_THREADS);
     int prev = 0;
     int i = 0;
     for (int t = 0; t < NUM_THREADS; t++) {
         long long curSum = 0;
-        while (curSum < target && i < work.size())
-            curSum += work[i++];
+        while (curSum < target && i < work.size()) {
+            curSum += work[i].second - work[i].first + 1;
+            i++;
+        }
         ret[t] = {prev, i};
         prev = i;
     }
@@ -69,7 +75,8 @@ signed main() {
     for (int &i : a) cin >> i;
     for (int &i : b) cin >> i;
 
-    vector<pair<int, int>> partitions = getPartitions();
+    int sum = computeWork();
+    vector<pair<int, int>> partitions = getPartitions(sum);
 
     auto start = chrono::high_resolution_clock::now();
     c.assign(n+m+1, 0);
